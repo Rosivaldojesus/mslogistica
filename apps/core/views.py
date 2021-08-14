12fdfe8cf2fdba30f05c9c57fd671b878fd7b2ce
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-08-14 08:06:21 -0300

    Primeiro Deploy

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from ..controle_vazios.models import ControleVazios
+from ..controle_fretes.models import ControleFretes
 
 # Create your views here.
 
 def Index(request):
-    return render(request, 'core/index.html')
+    quant_fretes = ControleFretes.objects.all().count()
+    quant_vazios = ControleVazios.objects.all().count()
+    return render(request, 'core/index.html', {
+        'quant_vazios':quant_vazios, 'quant_fretes':quant_fretes})
