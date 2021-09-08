a885fa91aee5679e61eac00c26d7a27109aaeeeb
Rosivaldojesus <js.rosivaldo@gmail.com>
2021-09-08 12:27:26 -0300

    make it better

--- apps/booking/admin.py
+++ apps/booking/admin.py
@@ -5,10 +5,10 @@ from .models import Booking
 
 
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'number_booking','status',)
+    list_display = ('id', 'number_booking','pol','pod','navio','status',)
     list_filter = ('number_booking','status',)
-    search_fields = ('number_booking', )
-    list_display_links =  ('number_booking','status',)
+    search_fields = ('number_booking','pol','pod','navio', )
+    list_display_links =  ('number_booking','pol','pod','navio','status',)
 
 
 admin.site.register(Booking, BookingAdmin)
